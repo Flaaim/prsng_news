e3bd153726723b8e321565b2b620fc799771a934
vagrant <vagrant@homestead>
2023-01-26 03:21:34 +0000

    create routing

--- public/index.php
+++ public/index.php
@@ -9,8 +9,13 @@ use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\Dispatcher;
 use App\NewsParce;
 
-
 $collector = new RouteCollector();
+
+function processInput($uri): string
+{
+    $uri = urldecode(parse_url($uri, PHP_URL_PATH));
+    return $uri;
+}
 
 $collector->get('/', function(){
     
@@ -21,6 +26,21 @@ $collector->get('/', function(){
     return $templates->render('news', ['news' => StorageNews::getNews()]);
 });
 
+$collector->get('/ini', function(){
+    return 'ini page';
+});
+
 $dispatcher =  new Dispatcher($collector->getData());
 
-echo $dispatcher->dispatch('GET', '/'), "\n";
+try {
+    $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], processInput($_SERVER['REQUEST_URI']));
+    //$dispatcher->dispatch('GET', '/ini'), "\n";
+}catch(Phroute\Phroute\Exception\HttpRouteNotFoundException $e){
+    echo $e->getMessage();
+    die();
+}catch(Phroute\Phroute\Exception\HttpMethodNotAllowedException $e){
+    echo $e->getMessage();
+    die();
+}
+echo $response;
+
